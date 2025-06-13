e026a28fd28778923519f0908825d16a12578de4
Simanta Paul <simanta.paul.26@gmail.com>
2025-06-13 17:23:02 +0600

    feat: add tailwind to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://ujjiboni-expreess.onrender.com',
+      "/api": {
+        target: "https://ujjiboni-expreess.onrender.com",
         changeOrigin: true,
         secure: false,
       },
     },
   },
-})
+});
